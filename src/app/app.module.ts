b7d13c1ff987a246e90f9787d2fe1de06a30f3cd
James Wesley Goedert <jwgoedert@gmail.com>
2017-05-25 00:37:53 -0500

    (feat) Add camera button and functionality to create page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
+import { Camera } from '@ionic-native/camera'; 
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -41,6 +42,7 @@ import { Config } from '../config';
     Config,
     StatusBar,
     SplashScreen,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/create-deck/create-deck.ts
+++ src/pages/create-deck/create-deck.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, Nav } from 'ionic-angular';
+import { NavController, Nav, AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-create-deck',
@@ -9,15 +11,64 @@ import { HomePage } from '../home/home';
 export class CreateDeckPage {
   @ViewChild(Nav) nav: Nav;
 
+  public photos: any;
+  public base64Image: string;
+
   rootPage: any = HomePage;
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, private camera: Camera, private alertCtrl: AlertController) {
+  }
+  ngOnInit() {
+    this.photos = [];
+  }
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    console.log("TOTOPHOTO");
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:  
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log('What is this?', this.base64Image);
+      this.photos.push(this.base64Image);
+      this.photos.reverse();
+    }, (err) => {
+      // Handle error
+    });
+  }
+  deletePhoto(index) {
+    // this.photos.splice(index, 1);
+    let confirm = this.alertCtrl.create({
+      title: 'Sure you want to delete this photo?',
+      message: '',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.photos.splice(index, 1);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateDeckPage');
-  }
 
-  click() {
-    console.log('they gone think i won a grammy!!!!!!')
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad CreateDeckPage');
+    }
+
+    click() {
+      console.log('they gone think i won a grammy!!!!!!')
+    }
   }
-}
